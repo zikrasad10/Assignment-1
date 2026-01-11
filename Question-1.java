6bf38ab67a891d70128f8f9efdf78f8292c2617e
zikrasad10 <zikrasad10@gmail.com>
2026-01-11 06:39:54 +0500

    Add files via upload

---
+++ Question-1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example input values (you can change these as needed)
+        double obtainedMarks = 450;
+        double maxMarks = 500;
+
+        double percentage = (obtainedMarks / maxMarks) * 100;
+        System.out.printf("Percentage: %.2f%%\n", percentage);
+    }
+}
+
